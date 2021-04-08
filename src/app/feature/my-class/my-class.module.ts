563c4b126624b9a40f6544602a25fc5738c6e1d4
adevadiga <abhinandhan.d@gmail.com>
2021-04-09 00:53:06 +0200

    Initial Project setup with Angular framework

---
+++ src/app/feature/my-class/my-class.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MyClassRoutingModule } from './my-class-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MyClassRoutingModule
+  ]
+})
+export class MyClassModule { }
